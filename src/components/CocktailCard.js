5720e4181c45b4d8237bd532f6a54eba35b5a09a
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-05 12:25:29 -0600

    fetch call for drink by name

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import '../scss/cocktailcard.scss';
 
 const CocktailCard = (props) => {
-	console.log('this is router props,', props);
-	console.log('this is props.match.params.cocktail,', props.match.params.cocktail);
+	// console.log('this is router props,', props);
+	// console.log('this is props.match.params.cocktail,', props.match.params.cocktail);
 	// let currentDrink = props.allDrinks.filter( (e) => {
 	//     console.log('this is e,', e)
 	//     return e
     // })
     
-const [currentDrink, setCurrentDrink] = useState('')
+const [currentDrink, setCurrentDrink] = useState([])
     
 //the idea here is that we get the currentDrink data from the click, and then call the cocktailDB api to match the name of the drink to a search, and then render those on the page
 //right now have a fetch set up
@@ -23,17 +23,22 @@ const [currentDrink, setCurrentDrink] = useState('')
                 let currentDrink = props.match.params;
                 console.log('this is current drink', currentDrink)
 				const response = await fetch(
-					`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${currentDrink}`
-				);
+					`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${currentDrink.cocktail}`
+                );
                 const data = await response.json();
-                //this comes out null for some reason
-                //once I can get this I can render everything on the page from the drink randomizer list
-				console.log('this is data', data);
+                console.log('this is data', data);
+                // this is an array
+                console.log('this is data.drinks', data.drinks)
+                //this is an object
+                console.log('this is data.drinks[0]', data.drinks[0])
+                console.log('this is data.drinks[0].strDrinkThumb', data.drinks[0].strDrinkThumb)
+                setCurrentDrink(data)
+                console.log('this is currentDrink', currentDrink)
             };
             getCurrentDrink()
 		}, []);
+//how to take the data I got above and render?
 
-    
     
     
 
