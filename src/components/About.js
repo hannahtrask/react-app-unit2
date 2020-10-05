53325321c77a22199cea0160b2167690bf4a8ad4
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-05 11:46:41 -0600

    photos added and about carousel

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../scss/about.scss';
+import {
+	Carousel,
+	CarouselItem,
+	CarouselIndicators,
+	CarouselCaption,
+} from 'reactstrap';
+
+const items = [
+	{
+		src:
+			require('../images/sam-hojati-pb7oJwtNVU4-unsplash.jpg'),
+		altText: 'cocktail 1',
+		caption: 'mint mojito',
+	},
+	{
+		src:
+			require('../images/ralph-ravi-kayden-shHq4kGdAaU-unsplash.jpg'),
+		altText: 'cocktail 2',
+		caption: 'martini',
+	},
+	{
+		src:
+			require('../images/adam-jaime-dmkmrNptMpw-unsplash.jpg'),
+		altText: 'cocktail 3',
+		caption: 'old-fashioned',
+	},
+];
 
 
-const About = () => {
+
+
+const About = (props) => {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+//why are photos not resizing to be larger?????
+const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
+
+
+
 	return (
 		<div>
-
-           <h1>about us</h1>
-           <p>either go to a randomly generated list of cocktails, or type in what you'd like in a form and we'll let you know how to make it</p>
-
+            <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+    </Carousel>
+			<h1>about us</h1>
+			<p>
+                our mission is to make a cocktail an art form again. expand your personal library by perusing a randomly generated list of cocktails by name, or type in what
+				you'd like above and we'll tell you how to make it!
+			</p>
 		</div>
 	);
 }
