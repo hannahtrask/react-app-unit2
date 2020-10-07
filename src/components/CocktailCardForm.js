9e2acd820d27833fb6c3432aa2724101128d6662
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-07 11:23:28 -0600

    cards for form search

--- src/components/CocktailCardForm.js
+++ src/components/CocktailCardForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../scss/cocktailcard.scss';
+import '../scss/cocktailcardform.scss';
 import '../scss/media_queries.scss';
 
 const CocktailCardForm = (props) => {
@@ -22,17 +22,57 @@ const drinkList = () => {
 	if (Array.isArray(drinkArr)) {
 		return drinkArr.map((bev) => {
             return (
-            <h1>{bev.strDrink}</h1>
-            
+            <div className='hero-div'>
+			<div className='cocktail-card'>
+				<img src={bev.strDrinkThumb} className='card' alt='drink image' />
+				<div class='container'>
+					<h3>your drink:</h3>
+					<h5>{bev.strDrink}</h5>
+					<br />
+					<h6>glassware: {bev.strGlass}</h6>
+				</div>
+			</div>
+			<div className='everything'>
+				<div className='ingredients'>
+					<p>
+						ingredient: {bev.strIngredient1}
+						<br />
+						amount: {bev.strMeasure1}
+					</p>
+					<p>
+						ingredient: {bev.strIngredient2}
+						<br />
+						amount: {bev.strMeasure2}
+					</p>
+					<p>
+						ingredient: {bev.strIngredient3}
+						<br />
+						amount: {bev.strMeasure3}
+					</p>
+					<p>
+						ingredient: {bev.strIngredient4}
+						<br />
+						amount: {bev.strMeasure4}
+					</p>
+				</div>
+				<div className='instructions'>
+					<h4>make it!</h4>
+					<p>{bev.strInstructions}</p>
+				</div>
+			</div>
+		</div>
             );
 		});
     }
 }
 
 
+
+
+
 	return (
-		<div className='cocktail-card'>
-            <h4>{drinkList()}</h4>
+		<div>
+            {drinkList()}
 		</div>
 	);
 };
