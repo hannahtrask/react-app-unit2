03957f5def266ec3cd51d7b454e70c2a9939cdcd
Hannah Catherine Trask <hctrask@gmail.com>
2020-11-08 14:17:38 -0700

    fixed some routing

--- src/App.js
+++ src/App.js
@@ -5,69 +5,75 @@ import CocktailList from './components/CocktailList';
 import CocktailForm from './components/CocktailForm';
 import CocktailCard from './components/CocktailCard';
 import CocktailCardForm from './components/CocktailCardForm';
-import {  Route, Link, Switch } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 
 function App() {
-const [selectedDrink, setSelectedDrink] = useState('');
-const [drink, setDrink] = useState('');
-const [drinkData, setDrinkData] = useState([]);
+	const [selectedDrink, setSelectedDrink] = useState('');
+	const [drink, setDrink] = useState('');
+	const [drinkData, setDrinkData] = useState([]);
 
-const handleClick = (drink) => {
-  setDrink(drink);
-}
+	const handleClick = (drink) => {
+		setDrink(drink);
+	};
 
-const handleSubmit = (bev) => {
-  console.log('this is handle submit')
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${bev}`)
-      .then((res) => res.json())
-      .then(res => setDrinkData(res.drinks))
-}
+	const handleSubmit = (bev) => {
+		console.log('this is handle submit');
+		fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${bev}`)
+			.then((res) => res.json())
+			.then((res) => setDrinkData(res.drinks));
+	};
 
-  return (
+	return (
 		<main>
-			<div className='App'>
-				<nav>
-					<ul className='about'>
-						<li>
-							<Link to='/'>
-								<h3>
-									Whiskey
-									<br />
-									Neat
-								</h3>
-							</Link>
-						</li>
-						<li>
-							<Link to='/about' className='link'>
-								about us
-							</Link>
-						</li>
-						<li>
-							<Link to='/cocktaillist' className='link'>
-								randomizer
-							</Link>
-						</li>
-					</ul>
-				</nav>
-				<div className='subheader'>
-					<p>a react app by hannah catherine trask</p>
-				</div>
-				<div className='siteName'>
-					<h1>Whiskey Neat</h1>
-				</div>
-				<CocktailForm handleSubmitFromApp={handleSubmit} />
-				<CocktailCardForm drinkData={drinkData} />
-				<Switch>
-					<Route exact path='/about'>
-						<About />
+			<Switch>
+				<div className='App'>
+					<nav>
+						<ul className='about'>
+							<li>
+								<Link to='/'>
+									<h3>
+										Whiskey
+										<br />
+										Neat
+									</h3>
+								</Link>
+							</li>
+							<li>
+								<Link to='/about' className='link'>
+									about us
+								</Link>
+							</li>
+							<li>
+								<Link to='/cocktaillist' className='link'>
+									randomizer
+								</Link>
+							</li>
+						</ul>
+					</nav>
+					<div className='subheader'>
+						<p>a react app by hannah catherine trask</p>
+					</div>
+					<div className='siteName'>
+						<h1>Whiskey Neat</h1>
+					</div>
+					<Route path='/'>
+						<CocktailForm handleSubmitFromApp={handleSubmit} />
 					</Route>
-					<Route exact path='/cocktaillist'>
-						<CocktailList
-							setSelected={(data) => {
-								setSelectedDrink(data);
-							}}
-						/>
-					</Route>
+					<Switch>
+						<Route exact path='/'>
+							<CocktailCardForm drinkData={drinkData} />
+						</Route>
+
+						<Route exact path='/about'>
+							<About />
+						</Route>
+						<Route exact path='/cocktaillist'>
+							<CocktailList
+								setSelected={(data) => {
+									setSelectedDrink(data);
+								}}
+							/>
+						</Route>
 						<Route
 							path='/cocktaillist/:cocktail'
 							render={(routerProps) => (
@@ -78,8 +84,9 @@ const handleSubmit = (bev) => {
 								/>
 							)}
 						/>
-				</Switch>
-			</div>
+					</Switch>
+				</div>
+			</Switch>
 		</main>
 	);
 }
--- src/components/CocktailForm.js
+++ src/components/CocktailForm.js
@@ -1,37 +1,44 @@
 import React, { useState } from 'react';
 import '../scss/cocktailform.scss';
 import '../scss/media_queries.scss';
+import { Link } from 'react-router-dom';
 
 const CocktailForm = (props) => {
-    const [cocktail, setCocktail] = useState('')
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        props.handleSubmitFromApp(cocktail)
-    }
-    const handleChange = (e) => {
-        e.preventDefault()
-        const bev = e.target.value
-        setCocktail(bev)
-    }
+	const [cocktail, setCocktail] = useState('');
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		props.handleSubmitFromApp(cocktail);
+	};
+	const handleChange = (e) => {
+		e.preventDefault();
+		console.log('this is e.target', e.target);
+		const bev = e.target.value;
+		setCocktail(bev);
+	};
 
-    return (
-			<div>
-				<div className='desktop'>
-					<h3>What'll you have?</h3>
-					<p>A resource for inspiration. Type in anything— want a margarita but not sure what spin to put on it? Not sure what you want to drink but know it has a 'B' somewhere in the name? Type anything below and we'll find something tasty for you.</p>
-				</div>
-				<form onSubmit={handleSubmit}>
-					<input
-						id='cocktail data'
-						type='string'
-						value={cocktail}
-						onChange={handleChange}
-					/>
-					<br />
-					<input className='submit' type='submit' value='find your cocktail' />
-				</form>
+	return (
+		<div>
+			<div className='desktop'>
+				<h3>What'll you have?</h3>
+				<p>
+					A resource for inspiration. Type in anything— want a margarita but not
+					sure what spin to put on it? Not sure what you want to drink but know
+					it has a 'B' somewhere in the name? Type anything below and we'll find
+					something tasty for you.
+				</p>
 			</div>
-		);
-}
+			<form onSubmit={handleSubmit}>
+				<input
+					id='cocktail data'
+					type='string'
+					value={cocktail}
+					onChange={handleChange}
+				/>
+				<br />
+				<input className='submit' type='submit' value='find your cocktail' />
+			</form>
+		</div>
+	);
+};
 
 export default CocktailForm;
